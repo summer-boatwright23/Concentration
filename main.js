98c5cb317319a7854c06af49f5ab6191c4817016
summer-boatwright <summerrane14@gmail.com>
2023-04-04 14:23:49 -0400

    added photos and wip: css

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 /*----- constants -----*/
-
-
-/*----- state variables -----*/
-
+//placement of cards
+//number of matches
 
 /*----- state variables -----*/
+//Number of lives 
 
 
 /*----- event listeners -----*/
+//a click on any of the cards
+//once a user has clicked two cards, they are unable to click anymore 
+//a matching pair has been found
+//an incompatible pair has been clicked
 
 
 /*----- functions -----*/
 
+//The browser will initailize and begin a game state automatically
 
+// I will need 16 cards to be showing face down when the game begins
+
+// they will all need to be clickable
+
+// the user will be able to select no more than two cards at a time
+
+//
+
+// 
+
+// 
+
+// 
+
+// 
+
+// 
+
+// 
