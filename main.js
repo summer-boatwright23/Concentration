1a9e3650ec04124c46c0823add7373ff3ef0bfe0
summer-boatwright <summerrane14@gmail.com>
2023-04-05 17:33:02 -0400

    afternoon push wip: JS

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 /*----- constants -----*/
 //placement of cards
-//number of matches
+const board = document.querySelector('#board');
 
 /*----- state variables -----*/
-//Number of lives 
-
+//number of matches
+let livesRemaining;
+let cards;
+let clicked;
+let winner;
+let timer;
+let matches;
 
 /*----- event listeners -----*/
 //a click on any of the cards
+
 //once a user has clicked two cards, they are unable to click anymore 
+
 //a matching pair has been found
+
 //an incompatible pair has been clicked
 
 
 /*----- functions -----*/
+initialize(); //this sets up the game when it loads in the browser
 
 //The browser will initailize and begin a game state automatically
 
@@ -22,17 +31,3 @@
 // they will all need to be clickable
 
 // the user will be able to select no more than two cards at a time
-
-//
-
-// 
-
-// 
-
-// 
-
-// 
-
-// 
-
-// 
