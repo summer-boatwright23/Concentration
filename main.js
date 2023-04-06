2874a5bf7d7caf527e96d13b5b3c056ab4e44f8b
summer-boatwright <summerrane14@gmail.com>
2023-04-06 19:38:36 -0400

    eod push, cards flip and only allow two at a time

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 /*----- constants -----*/
 //placement of cards
-const board = document.querySelector('board');
-const cards = document.querySelectorAll('flip-card')
+const board = document.querySelector('#board');
+const cards = document.querySelectorAll('.flip-card')
 /*----- state variables -----*/
 //number of matches
 let livesRemaining = 5;
-let cardClickOne = 1;
-let cardClickTwo = -1;
-let clicked = null;
-let winner;
-let timer;
-let matches;
+// let cardClickOne = 1;
+// let cardClickTwo = -1;
+ let chosen = [];
+let winner = false;
+// let timer;
+let matches = 0;
+let flip = 0;
 
 /*----- event listeners -----*/
 //a click on any of the cards
+board.addEventListener('click', flipCard);
+
+function flipCard(evt) {
+    const cardContainer = evt.target.closest('.flip-card-inner');
+    if (cardContainer && !cardContainer.classList.contains('flipped') && chosen.length < 2) {
+        cardContainer.style.transform = 'rotateY(180deg)';
+        cardContainer.classList.add('flipped');
+        chosen.push(cardContainer.dataset.id);
+        flip++;
+        if (chosen.length === 2) {
+          setTimeout(checkMatch, 1000);
+        }
+    }
+
+}
+
+
 
 //once a user has clicked two cards, they are unable to click anymore 
 
@@ -23,26 +41,23 @@ let matches;
 
 
 /*----- functions -----*/
-initialize(); //this sets up the game when it loads in the browser
+// initialize(); //this sets up the game when it loads in the browser
 
-function initailize() {
-    winner = null;
-    board = [
-        [0, 0, 0, 0], // col 0
-        [0, 0, 0, 0], // col 1
-        [0, 0, 0, 0], // col 2
-        [0, 0, 0, 0], // col 3
-        [0, 0, 0, 0]  // col 4
-     //  r0 r1 r2 r3   
-    ];
-    render();    
-}
+// function initailize() {
+//     winner = null;
+//     board = [
+//         [0, 0, 0, 0], // col 0
+//         [0, 0, 0, 0], // col 1
+//         [0, 0, 0, 0], // col 2
+//         [0, 0, 0, 0], // col 3
+//         [0, 0, 0, 0]  // col 4
+//      //  r0 r1 r2 r3   
+//     ];
+//     render();    
+// }
 
-function flipCard() {
-    this.classList.toggle('flip');
-}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+
 //The browser will initailize and begin a game state automatically
 
 // I will need 16 cards to be showing face down when the game begins
