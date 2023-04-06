2008f9e8b5b7e9a11bf55980a25aa40a46414128
summer-boatwright <summerrane14@gmail.com>
2023-04-06 16:00:28 -0400

    after lunch commit, tweaked css mostlyno bttn click

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 /*----- constants -----*/
 //placement of cards
-const board = document.querySelector('#board');
-
+const board = document.querySelector('board');
+const cards = document.querySelectorAll('flip-card')
 /*----- state variables -----*/
 //number of matches
-let livesRemaining;
-let cards;
-let clicked;
+let livesRemaining = 5;
+let cardClickOne = 1;
+let cardClickTwo = -1;
+let clicked = null;
 let winner;
 let timer;
 let matches;
@@ -24,6 +25,24 @@ let matches;
 /*----- functions -----*/
 initialize(); //this sets up the game when it loads in the browser
 
+function initailize() {
+    winner = null;
+    board = [
+        [0, 0, 0, 0], // col 0
+        [0, 0, 0, 0], // col 1
+        [0, 0, 0, 0], // col 2
+        [0, 0, 0, 0], // col 3
+        [0, 0, 0, 0]  // col 4
+     //  r0 r1 r2 r3   
+    ];
+    render();    
+}
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 //The browser will initailize and begin a game state automatically
 
 // I will need 16 cards to be showing face down when the game begins
