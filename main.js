33c8d160d2cf1d39d29d49edd7cb0bcd3ef93611
summer-boatwright <summerrane14@gmail.com>
2023-04-11 23:33:12 -0400

    cards flip, check for a mtch & flip back or stay facing depending

--- main.js
+++ main.js
@@ -15,14 +15,6 @@ let turns;
 //a click on any of the cards will result in it flipping
 board.addEventListener('click', flipCard);
 
-
-
-//once a user has clicked two cards, they are unable to click anymore 
-
-//a matching pair has been found
-
-//an incompatible pair has been clicked
-
 /*----- functions -----*/
 //this sets up the game when it loads in the browser
 function initialize(){
@@ -39,37 +31,45 @@ function initialize(){
           // flip the card by rotating it 180 degrees
          cardContainer.style.transform = 'rotateY(180deg)';
          // then mark the card as flipped
-        cardContainer.classList.add('flipped');
+         cardContainer.classList.add('flipped');
          // add the card's id to the list of chosen cards
-         chosen.push(cardContainer.dataset.id);
+         chosen.push(cardContainer);
          // increase the flip count by one
          flip++;
 
          // If two cards have been chosen, wait for a short delay and check if they match
          if (chosen.length === 2) {
            setTimeout(checkMatch, 1000);
+ 
         }
-     }
 
+    }
  }
 
-//fuction to check if a match is found
-function checkMatch() {
+//function to check if a match is found
+ function checkMatch() {
   const cards =  document.querySelectorAll('.flip-card-inner');
+  console.log(chosen)
   // get the first and second flipped card
-  const firstCard = cards[0];
-  const secondCard = cards[1];
+  const firstCard = chosen[0];
+  const secondCard = chosen[1];
+  console.log(firstCard.getAttribute('id'))
 
-  //If the framework of both cards are the same, add the 'matched' class and increment the matched counter
-  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+  //If the id of both cards are the same, add the 'matched' class and increment the matched counter
+  if (firstCard.getAttribute('id') === secondCard.getAttribute('id')) {
     firstCard.classList.add('matched');
     secondCard.classList.add('matched');
     matched++; // increase the matched counter
+    console.log('match')
   } 
   // else, remove the 'flipped' class from both cards, meaning they will flip back to their original state
   else {
+   if (firstCard.getAttribute('id') !== secondCard.getAttribute('id')) //
     firstCard.classList.remove('flipped');
     secondCard.classList.remove('flipped');
+    firstCard.style.transform = 'rotateY(0deg)';
+    secondCard.style.transform = 'rotateY(0deg)';
+    console.log('notmatch')
   }
 
   // Empty the chosen array so the player can choose new cards to flip
